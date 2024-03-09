95d01f20845187498b9d7c8c445628a85ec8d8ee
bestestest kitteh <236551@edu.p.lodz.pl>
2024-03-09 02:35:41 +0100

    implement barebones protocol

--- protocol.clj
+++ protocol.clj
@@ -1,4 +1,60 @@
 #!/usr/bin/env bb
-(ns protocol)
-(+ 3 5)
-(def a "awawa")
+(ns protocol
+  (:require [cheshire.core :as json]
+            [clojure.core.async :as a]))
+
+
+(def my-id (promise))
+(def nodes (promise))
+
+(defn start-reader []
+  (let [chan (a/chan)]
+    (a/go
+      (doseq [msg (json/parsed-seq *in* true)]
+        (a/>! chan [msg (:body msg) (-> msg :body :type)]))
+      (a/close! chan))
+    chan))
+
+(defn enumerate
+  [msg id]
+  (assoc-in msg [:body :msg_id] id))
+
+(defn start-writer []
+  (let [chan (a/chan)]
+    (a/go-loop [id 0]
+      (when-let [msg (a/<! chan)]
+        (->
+         msg
+         (enumerate id)
+         json/generate-string
+         println)
+        (recur (inc id))))
+    chan))
+
+(def reader (start-reader))
+(def writer (start-writer))
+
+(defn send-to [dest body]
+  {:dest dest
+   :body body})
+
+(defn reply-to [request body]
+  (let [dest (:src request)
+        request-id (-> request :body :msg_id)
+        body' (assoc body :in_reply_to request-id)]
+    (send-to dest body')))
+
+(defn error [request code text]
+  (reply-to request {:type "error"
+                     :code code
+                     :text text}))
+
+(defn send! [msg] (a/>!! writer msg))
+
+(defn initialize []
+  (let [[initial-msg body type] (a/<!! reader)
+        {:keys [node_id node_ids]} body]
+    (assert (= "init" type))
+    (deliver my-id node_id)
+    (deliver nodes node_ids)
+    (send! (reply-to initial-msg {:type "init_ok"}))))
